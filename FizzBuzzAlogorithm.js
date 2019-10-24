d55beb702570f9ca2ba9797c673b1cfd976c177d
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-24 15:15:22 +0200

    Added FizzBuzz

---
+++ FizzBuzzAlogorithm.js
@@ -0,0 +1,18 @@
+// FizzBuzzWork Algorithms program
+
+function fizzBuzzWork(Number)
+ {
+  for (var k = 1; k <= Number; k++)
+   {
+    if (k % 15 === 0)
+     console.log('FizzBuzz');
+    else if (k % 3 === 0) 
+     console.log('Fizz');
+    else if (k % 5 === 0) 
+    console.log('Buzz');
+    else 
+    console.log(k);
+  }
+}
+
+fizzBuzzWork(20);
