a6bfea3adaa23489326aa04d52d3a5305db2f5e3
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-24 15:48:02 +0200

    Added BubbleSortWork Algorithm

---
+++ BubbleSortAlogorithm.js
@@ -0,0 +1,17 @@
+// BubbleSortWork Algorithm
+
+function BubbleSortWork(ValueArray) {
+    for (var a = ValueArray.length; a > 0; a--) {
+      for (var b = 0; b < a; b++) {
+        if (ValueArray[b] > ValueArray[b + 1]) {
+          var temp = ValueArray[b];
+          ValueArray[b] = ValueArray[b + 1];
+          ValueArray[b + 1] = temp;
+        }
+      }
+    }
+    
+    return ValueArray;
+}
+ 
+BubbleSortWork([8000, 5000, 44, 303, 5, 646, 150, 884, 298, 76, 19, 3, 1]);;
